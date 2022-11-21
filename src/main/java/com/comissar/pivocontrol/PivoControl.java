2dacd3138862846749d199c5fdd3c84ae8b8a8eb
Comissar <73186419+Comissar1917@users.noreply.github.com>
2022-11-21 12:39:24 +0300

    Commit

---
+++ src/main/java/com/comissar/pivocontrol/PivoControl.java
@@ -0,0 +1,17 @@
+package com.comissar.pivocontrol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PivoControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/com/comissar/pivocontrol/listeners/ArmorSlotsListener.java
@@ -0,0 +1,7 @@
+package com.comissar.pivocontrol.listeners;
+
+import org.bukkit.event.Listener;
+
+public class ArmorSlotsListener implements Listener {
+
+}
