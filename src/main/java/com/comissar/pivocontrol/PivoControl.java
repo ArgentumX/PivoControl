92ef3307a752efe77f9b6d867cf580ef1dcabdc1
Comissar <73186419+Comissar1917@users.noreply.github.com>
2022-11-21 13:13:34 +0300

    Commit

--- src/main/java/com/comissar/pivocontrol/PivoControl.java
+++ src/main/java/com/comissar/pivocontrol/PivoControl.java
@@ -1,5 +1,7 @@
 package com.comissar.pivocontrol;
 
+import com.comissar.pivocontrol.listeners.ArmorSlotsListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PivoControl extends JavaPlugin {
@@ -7,6 +9,7 @@ public final class PivoControl extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        Bukkit.getPluginManager().registerEvents(new ArmorSlotsListener(this), this);
 
     }
 
--- src/main/java/com/comissar/pivocontrol/listeners/ArmorSlotsListener.java
+++ src/main/java/com/comissar/pivocontrol/listeners/ArmorSlotsListener.java
@@ -1,7 +1,27 @@
 package com.comissar.pivocontrol.listeners;
 
+import com.comissar.pivocontrol.PivoControl;
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockGrowEvent;
 
 public class ArmorSlotsListener implements Listener {
 
+    private PivoControl plugin;
+    public ArmorSlotsListener (PivoControl plugin){
+        this.plugin = plugin;
+    }
+    @EventHandler
+    public void OnSlot(PlayerArmorChangeEvent e) {
+
+        if (e.getNewItem().getType() == Material.ELYTRA) {
+            e.getPlayer().getWorld().dropItem(e.getPlayer().getLocation(), e.getNewItem());
+            e.getPlayer().getInventory().setChestplate(null);
+        }
+
+    }
 }
